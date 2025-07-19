4d7afd622e4028782e9e5ae551a8b97d3f15718d
Luis Pincay <lupin2201@gmail.com>
2025-07-19 10:20:50 -0500

    creación y primeros archivos en el repositorio

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = requiere('dotenv');
+const routes = require('./routes/index.routes');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 4001;
+
+app.use(cors());
+app.use(express.json());
+app.use('/api', routes);
+
+app.listen(port, () => {
+    console.log(`ITSUP API ejecutándose en https://api-itsup.luispincay.com:${port}`);
+});
