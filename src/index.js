8f96cfdae99c9b4862b987377e707bc752b9f603
Luis Pincay <lupin2201@gmail.com>
2025-07-19 11:44:53 -0500

    Instalación de módulos y dependencias

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-const dotenv = requiere('dotenv');
-const routes = require('./routes/index.routes');
+const dotenv = require('dotenv');
+//const routes = require('./routes/index.routes');
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4001;
 
-app.use(cors());
-app.use(express.json());
-app.use('/api', routes);
+// app.use(cors());
+// app.use(express.json());
+// app.use('/api', routes);
 
 app.listen(port, () => {
     console.log(`ITSUP API ejecutándose en https://api-itsup.luispincay.com:${port}`);
