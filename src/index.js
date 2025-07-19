f750f622618bf66e8e8965659ce2327d829a3bc7
Luis Pincay <lupin2201@gmail.com>
2025-07-19 12:01:18 -0500

    Configurando conexión con la BD

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const db = require('config/db.config');
 //const routes = require('./routes/index.routes');
 
 dotenv.config();
@@ -11,6 +12,15 @@ const port = process.env.PORT || 4001;
 // app.use(cors());
 // app.use(express.json());
 // app.use('/api', routes);
+
+//Conexión a la base de datos
+db.connect((err) => {
+    if (err) {
+        console.error('Error al conectar a la base de datos:', err);
+        process.exit(1);
+    }
+    console.log('Conexión a la base de datos establecida correctamente');
+});
 
 app.listen(port, () => {
     console.log(`ITSUP API ejecutándose en https://api-itsup.luispincay.com:${port}`);
