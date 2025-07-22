e81afc5fb8a75b3d9945bbac958b5b5ca6993ffd
Luis Pincay <lupin2201@gmail.com>
2025-07-22 11:06:37 -0500

    correrción generación token

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -3,8 +3,8 @@ const jwt = require('jsonwebtoken');
 // Middleware para verificar el token JWT
 const verifyToken = (req, res, next) => {
     // Obtener el token del header Authorization
-    const token = req.headers['authorization']; 
-    console.log('Token recibido:', token);
+    const token = req.headers['authorization']?.split(' ')[1]; 
+
     // Verificar si el token está presente
     if (!token) {
         console.log('Token no proporcionado');
