8981c31683b0492828de915b2c3c14d8c6e3ea94
Luis Pincay <lupin2201@gmail.com>
2025-07-23 16:13:40 -0500

    Testeo de operaciones para inscripción

--- src/models/inscripcion.model.js
+++ src/models/inscripcion.model.js
@@ -7,8 +7,8 @@ const Inscripcion = {
     },
 
     // Obtener inscripciones por periodo
-    findByIdPeriodo: (idmatricula, callback) => {
-        db.query("SELECT * FROM cese_inscripcion WHERE idmatricula = ?", [idmatricula], callback);
+    findByIdPeriodo: (idperiodo, callback) => {
+        db.query("SELECT * FROM cese_inscripcion WHERE idperiodo = ?", [idperiodo], callback);
     },
 
     // Buscar inscripción por matrícula
@@ -18,7 +18,9 @@ const Inscripcion = {
 
     // Crear una nueva inscripción
     createInscripcion: (inscripcionData, callback) => {
-        db.query("INSERT INTO cese_inscripcion SET ?", inscripcionData, callback);
+        const { idperiodo, idmatricula, fecha_inscripcion } = inscripcionData;
+
+        db.query("INSERT INTO cese_inscripcion (idperiodo, idmatricula, fecha_inscripcion) VALUES (?, ?, ?)", [idperiodo, idmatricula, fecha_inscripcion], callback);
     },
 
     // Obtener inscripción por ID
@@ -28,7 +30,9 @@ const Inscripcion = {
 
     // Actualizar una inscripción
     updateInscripcion: (idinscripcion, inscripcionData, callback) => {
-        db.query("UPDATE cese_inscripcion SET ? WHERE idinscripcion = ?", [inscripcionData, idinscripcion], callback);
+        var { idperiodo, idmatricula, fecha_inscripcion } = inscripcionData;
+
+        db.query("UPDATE cese_inscripcion SET idperiodo = ?, idmatricula = ?, fecha_inscripcion = ? WHERE idinscripcion = ?", [idperiodo, idmatricula, fecha_inscripcion, idinscripcion], callback);
     },
 
     // Eliminar una inscripción (marcar como inactiva)
