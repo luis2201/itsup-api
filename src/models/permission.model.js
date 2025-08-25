6657ed78cedb35275c5e6f84e82eec03cef1799b
Luis Pincay <lupin2201@gmail.com>
2025-08-25 16:57:36 -0500

    verificando rol que llega a permission model

--- src/models/permission.model.js
+++ src/models/permission.model.js
@@ -3,6 +3,7 @@ const db = require('../config/db');
 // Modelo para manejar permisos interactuando con la base de datos
 const Permissions = {
     getPermissionsByRole: (role, callback) => {
+        console.log(`Obteniendo permisos para el rol: ${role}`);
         db.query('SELECT * FROM tb_permisos WHERE Rol = ?', [role], callback);
     }
 };
