82e2266351cdc60bed7b8687a5bdafd3057c836c
Luis Pincay <lupin2201@gmail.com>
2025-08-27 16:53:06 -0500

    Testeando ruta Carrera

--- src/models/carrera.model.js
+++ src/models/carrera.model.js
@@ -1,5 +1,5 @@
 const db = require('../config/db');
-const { createCarrera } = require('../controllers/carrera.controller');
+const { createCarrera, getCarreraById, activarCarrera } = require('../controllers/carrera.controller');
 
 const Carrera = {
 
@@ -15,8 +15,25 @@ const Carrera = {
         const { carrera } = carreraData;
 
         db.query('INSERT INTO tb_carrera (carrera) VALUES (?)', [carrera], callback);
-    }
+    },
 
+    getCarreraById: (idcarrera, callback) => {
+        db.query('SELECT * FROM tb_carrera WHERE idcarrera = ?', [idcarrera], callback);
+    },
+
+    updateCarrera: (idcarrera, carreraData, callback) => {
+        const { carrera } = carreraData;
+
+        db.query('UPDATE tb_carrera SET carrera = ? WHERE idcarrera = ?', [carrera, idcarrera], callback);
+    },
+
+    deleteCarrera: (idcarrera, callback) => {
+        db.query('UPDATE tb_carrera SET estado = 0 WHERE idcarrera = ?', [idcarrera], callback);
+    },
+
+    activarCarrera: (idcarrera, callback) => {
+        db.query('UPDATE tb_carrera SET estado = 1 WHERE idcarrera = ?', [idcarrera], callback);
+    }
 };
 
 module.exports = Carrera;
